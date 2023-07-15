9a4cd2eb9c155edcc56796f04557e82eac10e119
Alexis Patiño Agudelo <alexispatinoagudelo@gmail.com>
2023-07-14 23:12:26 -0500

    feat(home,about): add home and about section

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,12 +7,22 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor(
     private translate: TranslateService
     ) {
     this.translate.setDefaultLang('es');
-    this.translate.use('es');
+    this.translate.use(this.currentLanguage);
+    this.translate.addLangs(['es', 'en']);
+    this.languages = this.translate.getLangs();
+    console.log(this.currentLanguage)
   }
 
+  currentLanguage: string = this.translate.getBrowserLang() ?? 'es';
+  languages: string[] = [];
+
+  changeLanguage(lang: string) {
+    console.log(lang)
+    this.translate.use(lang);
+    localStorage.setItem('Language', lang)
+  }
 }
